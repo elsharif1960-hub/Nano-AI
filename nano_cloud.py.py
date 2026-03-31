981577e60ea92ee6301ac2959cd472f80339b4a4
elsharif1960-hub <elsharif1960@gmail.com>
2026-03-31 17:25:32 +0200

    Update nano_cloud.py.py

--- nano_cloud.py.py
+++ nano_cloud.py.py
@@ -5,7 +5,7 @@ from groq import Groq
 st.set_page_config(page_title="Nano AI ⚡", layout="centered")
 
 # ضع مفتاح الـ API هنا (سأعلمك لاحقاً كيف تخفيه للأمان)
-client = Groq(api_key="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")
+client = Groq(api_key="Gsk_BML9Aa7F5Mmyds7FFcBvWGdyb3FYP7aXPXnvt2t7cXVS0vt1oEPi")
 
 st.markdown("""
     <style>
